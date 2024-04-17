4f312218be2040b7c6d58f2278a0f69498a08be8
nupshenoy <nupurshenoy@gmail.com>
2024-04-17 16:21:19 +0530

    version 2

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <h1> RR Handler</h1>
+      </nav>
+    </div>
+  )
+}
