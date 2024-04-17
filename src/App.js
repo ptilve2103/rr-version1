4d9ce53ea0092cddab5ac2c99da470457e06e460
nupshenoy <nupurshenoy@gmail.com>
2024-04-17 16:30:56 +0530

    verison 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Spreadsheet from 'react-spreadsheet';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App"><Navbar /></div>
-    
+  
+    const columnLabels = ["1", " 2", "3", "  4", "  5", "  6"];
+      const rowLabels = ["Aleema", "Naveen", "Nupur", "Amruta", "Lavanya", "Gaurav", "Anusha"];
+      const data = [
+        [{ value: "2024-0411-125787" }, { value: "" }],
+        [{ value: "" }, { value: "" }],
+      ];
+      return (
+        <BrowserRouter>
+        <div className="App">
+          <Navbar />
+        <Route path="/" element={<Spreadsheet
+        className='spreadsheet'
+          data={data}
+          columnLabels={columnLabels}
+          rowLabels={rowLabels}
+        />}/>
+        
+    </div>
+    </BrowserRouter>
   );
 }
 
